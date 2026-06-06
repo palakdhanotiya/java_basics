3b2667838fa3e302ec15ebc7ab10e40e59954f4b
Palak Dhanotiya <palakdhanotiya04@gmail.com>
2026-06-06 23:04:45 +0530

    Add files via upload

---
+++ StringLiteralEx.java
@@ -0,0 +1,12 @@
+public class StringLiteralEx {
+	public static void main(String[] args) {
+		String s="Palak Dhanotiya";
+		String ss="123";
+		String sss="33.56";
+		System.out.println(s);
+		System.out.println(ss);
+		System.out.println(sss);
+
+	}
+
+}
