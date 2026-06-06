93b1f8952f81b2bb320e56504fd52f4107a7858f
Palak Dhanotiya <palakdhanotiya04@gmail.com>
2026-06-06 23:03:34 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello{
+    public static void main(String args[]){
+         System.out.println("Hello World");
+         int a=10,b=20;
+         int c= a+b;
+         System.out.println("sum is "+c);
+    }
+}
+/*tokens= 52
+keywords =6
+identifiers = 16
+operators =5
+sepretors =21
+literals = 4*/
