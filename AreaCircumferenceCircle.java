70dd67530c10c10f57360ba1429571d0cb7ee795
Palak Dhanotiya <palakdhanotiya04@gmail.com>
2026-06-09 00:02:45 +0530

    AreaCircumferenceCircleEx

---
+++ AreaCircumferenceCircle.java
@@ -0,0 +1,13 @@
+public class AreaCircumferenceCircle {
+    public static void main(String[] args){
+      double r = 5;
+
+      double a = 3.14*r*r;
+      System.out.println("Area of circle with radius " + r + " is " + a);
+
+      double c = 2*3.14*r;
+      System.out.println("Circumference of circle with radius " + r + " is " + c);
+
+    }
+    
+}
