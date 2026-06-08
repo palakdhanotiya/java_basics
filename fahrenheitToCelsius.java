89b64ef82db24b0efda2e3fa5e98d65e6cc7448e
Palak Dhanotiya <palakdhanotiya04@gmail.com>
2026-06-09 00:02:00 +0530

    FahrenheitToCelsiusEx

---
+++ fahrenheitToCelsius.java
@@ -0,0 +1,7 @@
+public class fahrenheitToCelsius {
+    public static void main(String[] args){
+        double f = 66;
+        double c = 5*(f-32)/9;
+        System.out.println(f + " Degrees Fahrenheit is " +c + " Degrees Celsius");
+    }
+}
